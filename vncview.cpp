7f07faebbecd82d72f0f6f8f093b456273f41ce3
Edward Duffy <eduffy@clemson.edu>
2012-06-15 11:50:31 -0400

    don't #define error

--- vncview.cpp
+++ vncview.cpp
@@ -26,8 +26,8 @@
     #include <QMessageBox>
     #include <QInputDialog>
     #define KMessageBox QMessageBox
-    #define error(parent, message, caption) \
-        critical(parent, caption, message)
+//    #define error(parent, message, caption) \
+//        critical(parent, caption, message)
 #else
     #include "settings.h"
     #include <KActionCollection>
